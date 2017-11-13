5104611b6091fedf66833121d01a14bbc7f899bf
Fabio Guelfi <fabioguelfiunix@gmail.com>
2017-11-12 23:30:02 -0200

    traducao do imc para status

--- script.js
+++ script.js
@@ -4,8 +4,30 @@ $(function () {
         var altura = $('#altura').val();
         var peso = $('#peso').val();
 
+        altura = altura.replace(',', '.');
+        peso = peso.replace(',', '.');
+
         imc = peso / (altura*altura);
-        $('#resultado').html(imc);
+
+        if(imc < 16){ 
+            var traducao = ' baixo peso muito grave';
+        } else if (imc >= 16 && imc < 16.99) {
+            var traducao = 'baixo peso grave';
+        } else if (imc >= 17 && imc < 18.99) {
+            var traducao = 'baixo peso';
+        } else if (imc >= 18.50 && imc < 24.99){
+            var traducao = 'peso normal';
+        } else if (imc >= 25 && imc < 29.99){
+            var traducao = 'sobrepeso';
+        } else if (imc >= 30 && imc < 34,99){
+            var traducao = 'obesidade grau 1';
+        } else if (imc >= 35 && imc < 39,99){
+            var traducao = 'obesidade grau 2';
+        } else if (imc >= 40){
+            var traducao = 'obesidade grau 3';
+        }
+        
+        $('#resultado').html('Seu IMC '+imc.toFixed(2)+' e seu status: '+traducao);
     })
    
 });
